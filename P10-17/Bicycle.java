2a980b9f2a063f305ec5f95da22f0314733c6a1a
Matthew Layman <batmanlayman@gmail.com>
2015-12-10 20:51:06 -0500

    bicycle class

---
+++ P10-17/Bicycle.java
@@ -0,0 +1,6 @@
+//Matt Layman 10-17 Bicycle.java
+public class Bicycle implements CarbonFootprint {
+	public void GetCarbonFootprint() {
+		System.out.println("Bicycle: 0");
+	}
+} // end class Bicycle
